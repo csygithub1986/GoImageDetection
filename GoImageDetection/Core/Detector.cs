d5f42c05122246341ad4cd8a8e8fd57980e2673e
Csy <Csy@DELL-CSY>
2017-06-20 01:34:38 +0800

    改变线段检测参数

---
+++ GoImageDetection/Core/Detector.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Collections;
+
+namespace GoImageDetection.Core
+{
+    /// <summary>
+    /// 检测图形算法
+    /// 其中有2个假设：
+    /// 1、标准棋盘为0.9375:1，这里采用正方形输入。假定棋盘每一边都在(0.7~1) *(图宽)范围内
+    /// 2、棋盘交叉点误差应该小于最大格宽的正负5%；圆心坐标相差应小于最大格宽的正负25%
+    /// 目前采用整个棋盘全部检测模式
+    /// </summary>
+    public class Detector : IDetect
+    {
+        double minWidthRate = 0.7;
+        double cannyThreshold = 100;    // 参数5：边缘检测阈值（30~180）
+        double circleAccumulatorThreshold = 30;       // 参数6：累加器阈值（圆心重合点，越低的时候圆弧就越容易当成圆）
+
+        double dp = 1;    // 参数3：dp，不懂
+
+        int maxGridWidth;//最大格宽
+        int minGridWidth;//最小格宽
+
+        public Detector()
+        {
+
+        }
+
+        public CircleF[] Circles;
+        public List<Point> CrossPoints;
+
+        public int[] Detect(Bitmap bitmap, int boardSize)
+        {
+            UMat uimage = InitImage(bitmap);
+            maxGridWidth = uimage.Size.Width / (boardSize - 1);
+            minGridWidth = (int)(uimage.Size.Width / (boardSize - 1) * minWidthRate);
+
+            Circles = DetectCircle(uimage, boardSize);
+
+            List<LineSegment2D> horizontalLines;
+            List<LineSegment2D> verticalLines;
+            DetectLine(uimage, boardSize, out horizontalLines, out verticalLines);
+            CrossPoints = CalculateCross(horizontalLines, verticalLines);
+
+            return null;
+        }
+
+        /// <summary>
+        /// 图形预处理
+        /// </summary>
+        private UMat InitImage(Bitmap bitmap)
+        {
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(bitmap);
+            //转为灰度级图像
+            UMat uimage = new UMat();
+            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+            //use image pyr to remove noise 降噪，为了更准确的做边缘检测
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uimage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimage);
+            return uimage;
+        }
+
+        /// <summary>
+        /// 检测圆
+        /// </summary>
+        private CircleF[] DetectCircle(UMat uimage, int boardSize)
+        {
+            //棋子最大半径
+            int maxRadius = maxGridWidth / 2;//棋子最大宽度为格的二分之一
+            //棋子最小半径为最大半径*0.7
+            int minRadius = (int)(maxRadius * minWidthRate);
+            //最小间距为最小直径
+            int minDistance = minRadius * 2;
+            CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, dp, minDistance, cannyThreshold, circleAccumulatorThreshold, minRadius, maxRadius);
+            return circles;
+        }
+
+        /// <summary>
+        /// 检测线
+        /// </summary>
+        /// <param name="uimage"></param>
+        /// <param name="boardSize"></param>
+        /// <param name="horizontalLines"></param>
+        /// <param name="verticalLines"></param>
+        private void DetectLine(UMat uimage, int boardSize, out List<LineSegment2D> horizontalLines, out List<LineSegment2D> verticalLines)
+        {
+            UMat cannyEdges = new UMat();
+            //第三、四个参数分别为边缘检测阈值和连接阈值（大于第一个作为边界，小于第二个舍弃，介于之间时看该点是否连接着其他边界点）
+            CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThreshold * 0.8);
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //像素分辨率，取1
+               Math.PI / 180, //角度分辨率，取1度
+               (int)(uimage.Size.Width / (boardSize - 1) * minWidthRate * 0.8), //最小像素累计值，取最小线长*0.8
+               uimage.Size.Width / (boardSize - 1) * minWidthRate, //最小线长，取最小的棋子直径（如果一行当中只有一个空点，并且该点被左右两个棋子挤到不剩最小直径，有可能检测不到）
+               2); //找连续线，不允许间距，暂时允许2像素间距
+
+            //这里假设线的角度在14度以内，即边长1:4（已经很夸张了）
+            horizontalLines = new List<LineSegment2D>();
+            verticalLines = new List<LineSegment2D>();
+            foreach (var line in lines)
+            {
+                if (line.Direction.Y == 0 || Math.Abs(line.Direction.X / line.Direction.Y) > 4)
+                {
+                    horizontalLines.Add(line);
+                }
+                else if (line.Direction.X == 0 || Math.Abs(line.Direction.Y / line.Direction.X) > 4)
+                {
+                    verticalLines.Add(line);
+                }
+            }
+        }
+
+
+        private List<Point> CalculateCross(List<LineSegment2D> horizontalLines, List<LineSegment2D> verticalLines)
+        {
+            //去掉重复的线 TODO：这里就直接简单粗暴的认为5%内是同一根线，并取其中一根就可以，以后改进
+            List<LineSegment2D> distinctHorLines = new List<LineSegment2D>();
+            List<LineSegment2D> distinctVerLines = new List<LineSegment2D>();
+            foreach (var line in horizontalLines)
+            {
+                if (!distinctHorLines.Contains(line, new LineEqualityComparer<LineSegment2D>(minGridWidth * 0.05)))
+                {
+                    distinctHorLines.Add(line);
+                }
+            }
+            foreach (var line in verticalLines)
+            {
+                if (!distinctVerLines.Contains(line, new LineEqualityComparer<LineSegment2D>(minGridWidth * 0.05)))
+                {
+                    distinctVerLines.Add(line);
+                }
+            }
+            //计算交叉
+            List<Point> crossPointList = new List<Point>();
+            foreach (var horLine in distinctHorLines)
+            {
+                foreach (var verLine in distinctVerLines)
+                {
+                    PointF crossCenter = GetIntersection(horLine.P1, horLine.P2, verLine.P1, verLine.P2);
+                    if (!crossCenter.Equals(PointF.Empty))
+                    {
+                        crossPointList.Add(new Point((int)crossCenter.X, (int)crossCenter.Y));
+                    }
+                }
+            }
+            return crossPointList;
+        }
+
+
+        //判断两条线是否相交
+
+        double determinant(double v1, double v2, double v3, double v4)  // 行列式
+        {
+            return (v1 * v3 - v2 * v4);
+        }
+
+        bool IsIntersect(Point aP1, Point aP2, Point bP1, Point bP2)
+        {
+            double delta = determinant(aP2.X - aP1.X, bP1.X - bP2.X, aP2.Y - aP1.Y, bP1.Y - bP2.Y);
+            if (delta <= (1e-6) && delta >= -(1e-6))  // delta=0，表示两线段重合或平行
+            {
+                return false;
+            }
+            double namenda = determinant(bP1.X - aP1.X, bP1.X - bP2.X, bP1.Y - aP1.Y, bP1.Y - bP2.Y) / delta;
+            if (namenda > 1 || namenda < 0)
+            {
+                return false;
+            }
+            double miu = determinant(aP2.X - aP1.X, bP1.X - aP1.X, aP2.Y - aP1.Y, bP1.Y - aP1.Y) / delta;
+            if (miu > 1 || miu < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 计算两条直线的交点
+        /// </summary>
+        /// <param name="lineFirstStar">L1的点1坐标</param>
+        /// <param name="lineFirstEnd">L1的点2坐标</param>
+        /// <param name="lineSecondStar">L2的点1坐标</param>
+        /// <param name="lineSecondEnd">L2的点2坐标</param>
+        /// <returns></returns>
+        public static PointF GetIntersection(PointF lineFirstStar, PointF lineFirstEnd, PointF lineSecondStar, PointF lineSecondEnd)
+        {
+            /*
+             * L1，L2都存在斜率的情况：
+             * 直线方程L1: ( y - y1 ) / ( y2 - y1 ) = ( x - x1 ) / ( x2 - x1 ) 
+             * => y = [ ( y2 - y1 ) / ( x2 - x1 ) ]( x - x1 ) + y1
+             * 令 a = ( y2 - y1 ) / ( x2 - x1 )
+             * 有 y = a * x - a * x1 + y1   .........1
+             * 直线方程L2: ( y - y3 ) / ( y4 - y3 ) = ( x - x3 ) / ( x4 - x3 )
+             * 令 b = ( y4 - y3 ) / ( x4 - x3 )
+             * 有 y = b * x - b * x3 + y3 ..........2
+             * 
+             * 如果 a = b，则两直线平等，否则， 联解方程 1,2，得:
+             * x = ( a * x1 - b * x3 - y1 + y3 ) / ( a - b )
+             * y = a * x - a * x1 + y1
+             * 
+             * L1存在斜率, L2平行Y轴的情况：
+             * x = x3
+             * y = a * x3 - a * x1 + y1
+             * 
+             * L1 平行Y轴，L2存在斜率的情况：
+             * x = x1
+             * y = b * x - b * x3 + y3
+             * 
+             * L1与L2都平行Y轴的情况：
+             * 如果 x1 = x3，那么L1与L2重合，否则平等
+             * 
+            */
+            float a = 0, b = 0;
+            int state = 0;
+            if (lineFirstStar.X != lineFirstEnd.X)
+            {
+                a = (lineFirstEnd.Y - lineFirstStar.Y) / (lineFirstEnd.X - lineFirstStar.X);
+                state |= 1;
+            }
+            if (lineSecondStar.X != lineSecondEnd.X)
+            {
+                b = (lineSecondEnd.Y - lineSecondStar.Y) / (lineSecondEnd.X - lineSecondStar.X);
+                state |= 2;
+            }
+            switch (state)
+            {
+                case 0: //L1与L2都平行Y轴
+                    {
+                        if (lineFirstStar.X == lineSecondStar.X)
+                        {
+                            //throw new Exception("两条直线互相重合，且平行于Y轴，无法计算交点。");
+                            return PointF.Empty;
+                        }
+                        else
+                        {
+                            //throw new Exception("两条直线互相平行，且平行于Y轴，无法计算交点。");
+                            return PointF.Empty;
+                        }
+                    }
+                case 1: //L1存在斜率, L2平行Y轴
+                    {
+                        float x = lineSecondStar.X;
+                        float y = (lineFirstStar.X - x) * (-a) + lineFirstStar.Y;
+                        return new PointF(x, y);
+                    }
+                case 2: //L1 平行Y轴，L2存在斜率
+                    {
+                        float x = lineFirstStar.X;
+                        //网上有相似代码的，这一处是错误的。你可以对比case 1 的逻辑 进行分析
+                        //源code:lineSecondStar * x + lineSecondStar * lineSecondStar.X + p3.Y;
+                        float y = (lineSecondStar.X - x) * (-b) + lineSecondStar.Y;
+                        return new PointF(x, y);
+                    }
+                case 3: //L1，L2都存在斜率
+                    {
+                        if (a == b)
+                        {
+                            // throw new Exception("两条直线平行或重合，无法计算交点。");
+                            return PointF.Empty;
+                        }
+                        float x = (a * lineFirstStar.X - b * lineSecondStar.X - lineFirstStar.Y + lineSecondStar.Y) / (a - b);
+                        float y = a * x - a * lineFirstStar.X + lineFirstStar.Y;
+                        return new PointF(x, y);
+                    }
+            }
+            // throw new Exception("不可能发生的情况");
+            return PointF.Empty;
+        }
+
+
+        class LineEqualityComparer<T> : IEqualityComparer<T> where T : struct
+        {
+            double minDiff;//端点最小距离
+            public LineEqualityComparer(double minDiff)
+            {
+                this.minDiff = minDiff;
+            }
+            public bool Equals(T x, T y)
+            {
+                object objP1 = x;
+                LineSegment2D p1 = (LineSegment2D)objP1;
+                object objP2 = y;
+                LineSegment2D p2 = (LineSegment2D)objP2;
+                return ((p1.P1.X - p2.P1.X) * (p1.P1.X - p2.P1.X) + (p1.P1.Y - p2.P1.Y) * (p1.P1.Y - p2.P1.Y) < minDiff * minDiff &&
+                    (p1.P2.X - p2.P2.X) * (p1.P2.X - p2.P2.X) + (p1.P2.Y - p2.P2.Y) * (p1.P2.Y - p2.P2.Y) < minDiff * minDiff);
+            }
+
+            public int GetHashCode(T obj)
+            {
+                return 0;
+            }
+        }
+    }
+}
--- GoImageDetection/MainWindow.xaml.cs
+++ GoImageDetection/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Emgu.CV.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,19 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Diagnostics;
+using Emgu.CV.Util;
+using GoImageDetection.Core;
+using System.Drawing;
 
 namespace GoImageDetection
 {
@@ -19,9 +28,166 @@ namespace GoImageDetection
     /// </summary>
     public partial class MainWindow : Window
     {
+        double dp = 1;    // 参数3：dp，不懂
+        double minDist = 40;     // 参数4：两个圆中心的最小距离
+        double cannyThreshold = 80;    // 参数5：边缘检测阈值（30~180）
+        double circleAccumulatorThreshold = 30;       // 参数6：累加器阈值（圆心重合点，越低的时候圆弧就越容易当成圆）
+        int minRadius = 20;
+        int maxRadius = 60;
+
+        double lineDp = 2;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            txtDp.Text = dp.ToString();
+            txtCanny.Text = cannyThreshold.ToString();
+            txtAccThr.Text = circleAccumulatorThreshold.ToString();
+            txtMinDist.Text = minDist.ToString();
+            txtMinCir.Text = minRadius.ToString();
+            txtMaxCir.Text = maxRadius.ToString();
+            txtLineDp.Text = lineDp.ToString();
+        }
+
+        private void BtnReDetect_Click(object sender, RoutedEventArgs e)
+        {
+            dp = int.Parse(txtDp.Text.Trim());
+            cannyThreshold = int.Parse(txtCanny.Text.Trim());
+            circleAccumulatorThreshold = int.Parse(txtAccThr.Text.Trim());
+            minDist = int.Parse(txtMinDist.Text.Trim());
+            minRadius = int.Parse(txtMinCir.Text.Trim());
+            maxRadius = int.Parse(txtMaxCir.Text.Trim());
+            lineDp = double.Parse(txtLineDp.Text.Trim());
+
+            PerformShapeDetection();
+        }
+
+
+
+        public void PerformShapeDetection()
+        {
+            try
+            {
+                if (fileNameTextBox.Text != String.Empty)
+                {
+                    #region 预处理
+
+                    StringBuilder msgBuilder = new StringBuilder("Performance: ");
+
+                    //Load the image from file and resize it for display
+                    Image<Bgr, Byte> img = new Image<Bgr, byte>(fileNameTextBox.Text);
+                    //.Resize(2000, 2000, Emgu.CV.CvEnum.Inter.Linear, true);
+                    //labelSize.Text = img.Width.ToString() + " , " + img.Height.ToString();
+                    //转为灰度级图像
+                    UMat uimage = new UMat();
+                    CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+
+                    //use image pyr to remove noise 降噪，为了更准确的做边缘检测
+                    UMat pyrDown = new UMat();
+                    CvInvoke.PyrDown(uimage, pyrDown);
+                    CvInvoke.PyrUp(pyrDown, uimage);
+                    #endregion
+
+
+                    #region 检测圆
+                    Stopwatch watch = Stopwatch.StartNew();
+                    CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, dp, minDist, cannyThreshold, circleAccumulatorThreshold, minRadius, maxRadius);
+
+                    watch.Stop();
+                    msgBuilder.Append(String.Format("Hough circles - {0} ms; ", watch.ElapsedMilliseconds));
+                    #endregion
+
+                    #region Canny and edge detection
+                    watch.Reset(); watch.Start();
+                    double cannyThresholdLinking = cannyThreshold / 2;// 30;
+                    UMat cannyEdges = new UMat();
+                    CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThresholdLinking);
+
+                    LineSegment2D[] lines = CvInvoke.HoughLinesP(
+                       cannyEdges,
+                       lineDp, //Distance resolution in pixel-related units
+                       Math.PI / 720, //Angle resolution measured in radians.
+                       50, //threshold
+                       50, //min Line width
+                       20); //gap between lines
+
+                    watch.Stop();
+                    msgBuilder.Append(String.Format("Canny & Hough lines - {0} ms; ", watch.ElapsedMilliseconds));
+                    imageOrigin.Image = img;
+                    #endregion
+
+                    #region draw circles
+                    Mat circleImage = new Mat(img.Size, DepthType.Cv8U, 3);
+                    circleImage.SetTo(new MCvScalar(0));
+                    foreach (CircleF circle in circles)
+                        CvInvoke.Circle(circleImage, System.Drawing.Point.Round(circle.Center), (int)circle.Radius, new Bgr(System.Drawing.Color.Brown).MCvScalar, 2);
+
+                    imageCircles.Image = circleImage;
+                    #endregion
+
+                    #region draw lines
+                    Mat lineImage = new Mat(img.Size, DepthType.Cv8U, 3);
+                    lineImage.SetTo(new MCvScalar(0));
+                    System.Drawing.Color[] colors = {
+                        System.Drawing.Color.Red,
+                        System.Drawing.Color.Orange,
+                        System.Drawing.Color.Yellow,
+                        System.Drawing.Color.Green,
+                        System.Drawing.Color.Blue,
+                        System.Drawing.Color.Purple
+                    };
+                    int index = 0;
+                    foreach (LineSegment2D line in lines)
+                    {
+                        if (index> colors.Length-1)
+                        {
+                            index = 0;
+                        }
+                        CvInvoke.Line(circleImage, line.P1, line.P2, new Bgr(colors[index++]).MCvScalar, 2);
+                    }
+
+                    imageOrigin.Image = cannyEdges;
+                    imageResult.Image = circleImage;
+                    #endregion
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK || result == System.Windows.Forms.DialogResult.Yes)
+            {
+                fileNameTextBox.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void BtnReDetect_Click2(object sender, RoutedEventArgs e)
+        {
+            Detector detecotr = new Detector();
+            Bitmap bitmap = new Bitmap(fileNameTextBox.Text);
+            detecotr.Detect(bitmap, 19);
+
+
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(bitmap);
+            Mat circleImage = new Mat(img.Size, DepthType.Cv8U, 3);
+            circleImage.SetTo(new MCvScalar(0));
+            foreach (CircleF circle in detecotr.Circles)
+                CvInvoke.Circle(circleImage, System.Drawing.Point.Round(circle.Center), (int)circle.Radius, new Bgr(System.Drawing.Color.Brown).MCvScalar, 2);
+
+            foreach (var cross in detecotr.CrossPoints)
+            {
+                CvInvoke.Circle(circleImage, cross, 2, new Bgr(System.Drawing.Color.Green).MCvScalar, 2);
+            }
+            imageOrigin.Image = img;
+            imageResult.Image = circleImage;
         }
     }
 }
